7616e841f71ad11f8b1660b8494cf1f68c5e26b0
shuntaro-fukuta <shuntaro.fkt@gmail.com>
2022-09-13 16:56:57 +0900

    Multiples of 3 or 5

---
+++ codewars/6kyu/Multiples_of_3_or_5.php
@@ -0,0 +1,22 @@
+// https://www.codewars.com/kata/514b92a657cdc65150000006/train/php
+
+function solution($number){
+  if ($number == 0) return 0;
+
+  $sum = 0;
+  for ($i = 1; $i < $number; $i++) {
+    $temp = $number - $i;
+    
+    if ($temp % 3 == 0 && $temp % 5 == 0) {
+      $sum += $temp;
+      continue;
+    }
+    
+    if ($temp % 3 == 0 || $temp % 5 == 0) {
+      $sum += $temp;
+    }
+  }
+  
+  return $sum;
+}
+
