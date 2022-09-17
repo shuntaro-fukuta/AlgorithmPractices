e5c3ce28d63899631ca5d08f8ac8c10ae973c0cd
shuntaro-fukuta <shuntaro.fkt@gmail.com>
2022-09-17 19:36:40 +0900

    Two Sum

---
+++ leetcode/easy/TwoSum.java
@@ -0,0 +1,24 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[0] = map.get(target - nums[i]);
+                result[1] = i;
+                return result;
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return result;
+    }
+}
+
